40842f12c8eeadb5713556471464b562e48c2144
Andre Le <andre@andrele.com>
2013-11-16 01:10:58 -0500

    Added tabs

--- src/edu/cmu/andrele/contextualcontacts/MainActivity.java
+++ src/edu/cmu/andrele/contextualcontacts/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.Window;
+import android.widget.TabHost;
+import android.widget.TabHost.*;
 
 public class MainActivity extends Activity {
 
@@ -13,6 +15,20 @@ public class MainActivity extends Activity {
 		// Remove the title bar
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
+		
+		TabHost tabHost=(TabHost)findViewById(android.R.id.tabhost);
+		tabHost.setup();
+
+		TabSpec spec1=tabHost.newTabSpec("Add Contact");
+		spec1.setContent(R.id.tab1);
+		spec1.setIndicator("Add Contact");
+
+		TabSpec spec2=tabHost.newTabSpec("History");
+		spec2.setIndicator("History");
+		spec2.setContent(R.id.tab2);
+
+		tabHost.addTab(spec1);
+		tabHost.addTab(spec2);
 	}
 
 	@Override
