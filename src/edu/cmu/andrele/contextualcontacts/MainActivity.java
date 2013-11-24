6971a01fb4e036d21d6a8939ecc7b59d543ab237
Andre Le <andre@andrele.com>
2013-11-24 13:58:30 -0500

    Google play location works on device. Not emulator

--- src/edu/cmu/andrele/contextualcontacts/MainActivity.java
+++ src/edu/cmu/andrele/contextualcontacts/MainActivity.java
@@ -96,7 +96,7 @@ GooglePlayServicesClient.OnConnectionFailedListener {
      * Constants for location update parameters
      */
     public static final int MILLISECONDS_PER_SECOND = 1000;
-    public static final int UPDATE_INTERVAL_IN_SECONDS = 5;
+    public static final int UPDATE_INTERVAL_IN_SECONDS = 300;
     public static final int FAST_CEILING_IN_SECONDS = 1;
     public static final long UPDATE_INTERVAL_IN_MILLISECONDS = MILLISECONDS_PER_SECOND * UPDATE_INTERVAL_IN_SECONDS;
     public static final long FAST_INTERVAL_CEILING_IN_MILLISECONDS = MILLISECONDS_PER_SECOND * FAST_CEILING_IN_SECONDS;
